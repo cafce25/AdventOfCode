2ac72e19817da28ee04b1f4a79afe8bc00c6c3d7
cafce25 <cafce25@gmail.com>
2020-12-05 01:33:18 +0100

    Add solutions for day 3 puzzles

---
+++ 2020/3/treesHit.hs
@@ -0,0 +1,10 @@
+module Main where
+
+main = interact $ show . length . treesOnSlope 0 1 2 . map cycle . lines
+
+treeAt start shift (i, row) = '#' == row !! (start + shift * i)
+
+treesOnSlope start right down = (filter (treeAt start right)) . zip [0..] . everyN down
+
+everyN n [] = []
+everyN n (x:xs) = x : everyN n (drop (n-1) xs)
---
+++ 2020/3/treesHit2.hs
@@ -0,0 +1,16 @@
+module Main where
+
+main = interact $ show . multiTrees . map cycle . lines
+
+multiTrees trees = treesOnSlope 0 1 1 trees
+                 * treesOnSlope 0 3 1 trees
+                 * treesOnSlope 0 5 1 trees
+                 * treesOnSlope 0 7 1 trees
+                 * treesOnSlope 0 1 2 trees
+
+treeAt start shift (i, row) = '#' == row !! (start + shift * i)
+
+treesOnSlope start right down = length . (filter (treeAt start right)) . zip [0..] . everyN down
+
+everyN n [] = []
+everyN n (x:xs) = x : everyN n (drop (n-1) xs)
