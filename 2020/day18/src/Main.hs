4218924b6d52d9f32e647b25e7ac366fb5203b91
Jonathan Birk <jona@jbirk.de>
2020-12-18 22:26:55 +0100

    2020 day 18 part 1

---
+++ 2020/day18/src/Main.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE ApplicativeDo     #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module Main where
+
+import           Control.Arrow ((&&&))
+import           Control.Monad.Combinators.Expr
+import           Data.Either (rights)
+import           Data.Text (Text)
+import qualified Data.Text as T
+import           Data.Void
+import           System.Environment (getArgs)
+import           Text.Megaparsec hiding (getInput)
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Input = [Expr]
+type Parser = Parsec Void Text
+data Expr = Int Int
+          | Sum     Expr Expr
+          | Product Expr Expr
+          deriving (Eq, Ord, Show)
+
+sc :: Parser ()
+sc = L.space space1 empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+value :: Expr -> Int
+value (Int i) = i
+value (Sum     e1 e2) = value e1 + value e2
+value (Product e1 e2) = value e1 * value e2
+
+pInteger :: Parser Expr
+pInteger = Int <$> lexeme L.decimal
+
+pTerm :: Parser Expr
+pTerm = choice
+    [ parens pExpr
+    , pInteger
+    ]
+
+pExpr :: Parser Expr
+pExpr = makeExprParser pTerm operatorTable
+
+operatorTable :: [[Operator Parser Expr]]
+operatorTable =
+    [ [ binary "*" Product
+      , binary "+" Sum
+      ]
+    ]
+
+binary :: Text -> (Expr -> Expr -> Expr) -> Operator Parser Expr
+binary name f = InfixL (f <$ symbol name)
+
+part1 :: Input -> Int
+part1 = sum . map value
+
+part2 :: Input -> ()
+part2 = const ()
+
+prepare :: String -> Input
+prepare = rights. map (parse pExpr "". T.pack) . lines 
+
+main :: IO ()
+main = getInput >>= print . (part1 &&& part2) . prepare
+
+getInput :: IO String
+getInput = do
+    args <- getArgs
+    case args of
+        []    -> readFile "input"
+        ["-"] -> getContents
+        fs    -> concat `fmap` mapM readFile fs
