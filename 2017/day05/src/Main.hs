74b6b0dc8862c733f231986e115f0aee69f2a11c
Jonathan Birk <jona@jbirk.de>
2020-12-23 13:18:38 +0100

    2017 day 5 part 2

--- 2017/day05/src/Main.hs
+++ 2017/day05/src/Main.hs
@@ -4,30 +4,37 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.IntMap (IntMap, (!))
+import qualified Data.IntMap as M
 import System.Environment (getArgs)
 
-type Input = [Int]
+type Input = IntMap Int
 
-jump :: Int -> [Int] -> (Int, [Int])
+jump :: Int -> Input -> (Int, Input)
 jump pos ls = (el + pos, new)
-    where new = b ++ [succ el] ++ tail a
-          (b, a) = splitAt pos ls
-          el = ls !! pos
+    where new = M.insert pos (succ el) ls
+          el = ls ! pos
 
-steps :: [Int] -> Int
-steps = steps' 0 0
+steps :: (Int -> Input -> (Int, Input)) -> Input -> Int
+steps jumpf = steps' 0 0
     where steps' n pos ls = if pos >= length ls || pos < 0
                                then n
-                               else uncurry (steps' (succ n)) $ jump pos ls
+                               else uncurry (steps' (succ n)) $ jumpf pos ls
 
 part1 :: Input -> Int 
-part1 = steps
+part1 = steps jump
 
-part2 :: Input -> ()
-part2 = const ()
+jump2 :: Int -> Input -> (Int, Input)
+jump2 pos ls = (el + pos, new)
+    where new = M.insert pos (nextEl) ls
+          el = ls ! pos
+          nextEl = if el >= 3 then pred el else succ el
+
+part2 :: Input -> Int
+part2 = steps jump2
 
 prepare :: String -> Input
-prepare = map read . lines
+prepare = M.fromList . zip [0..] . map read . lines
 
 main :: IO ()
 main = getInput >>= print . (part1 &&& part2) . prepare
