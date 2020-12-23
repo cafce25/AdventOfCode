9aaca13dc4927a87d767973b726d25debfac3f18
Jonathan Birk <jona@jbirk.de>
2020-12-23 14:32:16 +0100

    2017 day 6 part 1

---
+++ 2017/day06/src/Main.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Main where
+
+import Control.Arrow ((&&&))
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as M
+import Data.List (find)
+import Data.Maybe (fromJust)
+import qualified Data.Set as S
+import System.Environment (getArgs)
+
+type Input = IntMap Int
+
+distribute :: Input -> Input
+distribute banks = M.mapWithKey distribute' banks
+    where maxEl = maximum banks
+          a = M.assocs banks
+          maxI = fst . fromJust . find ((maxEl==) . snd) $ a
+          (q, r) = maxEl `divMod` l
+          l = length banks
+          distribute' k v
+              | k == maxI = q
+              | maxI + r >= k && k > maxI
+                  || maxI + r >= l && k <= (maxI + r) `mod` l = v + q + 1
+              | otherwise = v + q
+
+part1 :: Input -> Int
+part1 = length . takeWhileNoDupe S.empty . map M.elems . iterate distribute  
+    where takeWhileNoDupe _ [] = [] 
+          takeWhileNoDupe acc (x:xs) = if x `S.member` acc
+                                     then []
+                                     else x:(takeWhileNoDupe (S.insert x acc) xs )
+
+part2 :: Input -> ()
+part2 = const ()
+
+prepare :: String -> Input
+prepare = M.fromList . zip [0..] . map read . words
+
+main :: IO ()
+main = getInput >>= print . (part1 &&& part2) . prepare
+
+getInput :: IO String
+getInput = do
+    args <- getArgs
+    case args of
+        []    -> readFile "input"
+        ["-"] -> getContents
+        fs    -> concat `fmap` mapM readFile fs
